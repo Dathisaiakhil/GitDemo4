ba9448be649e09c4b3f7764c0cf611a53e3b4bdd
SaiAkhil <dathi30081997@gmail.com>
2021-11-10 12:09:54 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Circle
+{
+    static float PI;
+    int _radius;
+
+    static Circle()
+    {
+        PI = 3.14f;
+    }
+
+    public Circle(int radius)
+    {
+        _radius = radius;
+    }
+
+    public double Area()
+    {
+        return PI * _radius * _radius;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Circle c1 = new Circle(5);
+        Console.WriteLine("Area is : {0}", c1.Area());
+
+        Circle c2 = new Circle(6);
+        Console.WriteLine("Area is : {0}", c2.Area());
+    }
+}
