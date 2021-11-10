bd8a8e1579e6aafcc07c25bb429c608bef2a5d7e
SaiAkhil <dathi30081997@gmail.com>
2021-11-10 12:13:00 +0530

    new changes

--- Program.cs
+++ Program.cs
@@ -29,5 +29,8 @@ class Program
 
         Circle c2 = new Circle(6);
         Console.WriteLine("Area is : {0}", c2.Area());
+
+        Circle c3 = new Circle(7);
+        Console.WriteLine("Area is : {0}", c3.Area());
     }
 }
